a7a6023731b5fffda3d4a1b450fba45ea1c5e156
Jason Wu <lujason2015@gmail.com>
2018-06-06 22:13:48 -0400

    jason test

--- scripts/tradetracker.js
+++ scripts/tradetracker.js
@@ -1,6 +1,10 @@
-
-//Get Text from inputer box
+$(document).ready(function(){
+    //Get Text from inputer box
 document.getElementById('search').addEventListener('click', function(){
  var sText=document.getElementById('sBox').value
 });
+    
+    });
+
+
 
