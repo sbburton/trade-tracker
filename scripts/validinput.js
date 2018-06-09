ff0400c2cd2edcde03a7c912978de7f24df32e91
klrqn <burtonsb@gmail.com>
2018-06-09 18:09:27 +0000

    fixed function call validinput

--- scripts/validinput.js
+++ scripts/validinput.js
@@ -5,37 +5,33 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("submit").addEventListener("click", function(e){
     e.preventDefault()//prevent page refresh
 
-		function checkValidity() {
-
-			var symbol = $("#symbol").val()
-			var company = $("#company").val()
-			var price = $("#price").val()
-			var client = $("#clientName").val()
-			var date = $("#date").val()
-			var broker = $("#broker").val()
-			var status = $("#status").val()
-
-			var inputList = ['symbol', 'company', 'price', 'clientName', 'date', 'broker', 'status'];
-			// console.log(`symbol: ${symbol}, company: ${company}, price: ${price}, client: ${client}, broker: ${broker}, status: ${status}`);
-
-			var errorCount = 0;
-			for (var i=0; i<inputList.length; i++){
-				if ($(`#${inputList[i]}`).val() == "" && errorCount === 0 && $("#error").text() != "Please fill out all fields!") {
-					console.log(inputList[i], ' blank');
-					$("#addItem").append("<p id='error' style='font-weight:bold; color:red; text-shadow:1px 1px #ffddbb;'>Please fill out all fields!</p>");
-					errorCount++;
-				}	else if (errorCount == 0) {
-					$("#error").remove();
-				}
-			}
-
-
-		}
-
-		// var inputForm = $("#addItem")
-		// console.log('inputForm', inputForm);
-		// console.log('inputForm.html()', inputForm.html());
+	checkValidity();
 
 	});
+
+	function checkValidity() {
+
+		var symbol = $("#symbol").val()
+		var company = $("#company").val()
+		var price = $("#price").val()
+		var client = $("#clientName").val()
+		var date = $("#date").val()
+		var broker = $("#broker").val()
+		var status = $("#status").val()
+
+		var inputList = ['symbol', 'company', 'price', 'clientName', 'date', 'broker', 'status'];
+		// console.log(`symbol: ${symbol}, company: ${company}, price: ${price}, client: ${client}, broker: ${broker}, status: ${status}`);
+
+		var errorCount = 0;
+		for (var i=0; i<inputList.length; i++){
+			if ($(`#${inputList[i]}`).val() == "" && errorCount === 0 && $("#error").text() != "Please fill out all fields!") {
+				console.log(inputList[i], ' blank');
+				$("#addItem").append("<p id='error' style='font-weight:bold; color:red; text-shadow:1px 1px #ffddbb;'>Please fill out all fields!</p>");
+				errorCount++;
+			} else if (errorCount == 0) {
+				$("#error").remove();
+			}
+		}
+	}
 
 });
