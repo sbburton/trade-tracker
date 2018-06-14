d84685a7d0b68c13ba096e07b07a30afda6dd5c5
lizprogrammer <lizprogrammer@yahoo.com>
2018-06-14 08:03:17 -0400

    Completed search function

--- scripts/search.js
+++ scripts/search.js
@@ -3,11 +3,47 @@ const list=document.querySelector('#stockTable');
 
 //filter Books
 const searchBar=document.forms['search-stocks'].querySelector('input');
+//console.log(searchBar);
 
 searchBar.addEventListener('keyup',function(e){
 
-    var myList = document.getElementById("stockTable").childNodes.length;
-    console.log(myList);
+    var myList = document.getElementById("stockTable").childNodes;
+//    console.log(myList.length);
+
+    var searchString = document.getElementById("search").value;
+    var str = '';
+
+    console.log(myList.length);
+
+    for(var i = 1; i < myList.length; i++){
+
+        str = myList[i].firstElementChild.innerHTML;
+
+        str = str.toUpperCase();
+        searchString = searchString.toUpperCase();
+
+        if(searchString.length == 0)
+        {
+            document.getElementById("stockTable").childNodes[i].hidden = false;
+        }
+        else{
+            if(str.indexOf(searchString) != -1)
+            {
+                //console.log("Search index: " + str.indexOf(searchString));
+                console.log("Found it! " + str + " " + searchString);
+                document.getElementById("stockTable").childNodes[i].hidden = false;
+            }
+            else
+            {
+                //hide the row
+                console.log("Not a match: " + str + " " + searchString);
+                document.getElementById("stockTable").childNodes[i].hidden = true;
+            }
+        }
+    }
+
+    //console.log(searchString);
+    //console.log(str);
 
     const term=e.target.value.toLowerCase();
     const stocks=list.getElementsByTagName('li');
