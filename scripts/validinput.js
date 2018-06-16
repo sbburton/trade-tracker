00c6351be2455fbe2cde7a9c256a10d7a53789df
Jim Doyle <indellibleink@gmail.com>
2018-06-16 02:18:32 -0400

    corrected typo

--- scripts/validinput.js
+++ scripts/validinput.js
@@ -37,6 +37,6 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Gather Stock List and Validate it
-$.getJSON("ajax/NASDAQ.json", futknction(stkInfo){
+$.getJSON("ajax/NASDAQ.json", function(stkInfo){
 		console.log(stkInfo);				//  Display List of ID and Objets to console,
 });
