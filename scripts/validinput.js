54d1fcf1fa70dd0e27bc6361f5959754e485a5b9
Elizabeth Rosso <lizprogrammer@intracitygeeks.org>
2018-06-16 12:31:01 -0400

    Changed fields' type to number in index.html

--- scripts/validinput.js
+++ scripts/validinput.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		var symbol = $("#symbol").val()
 		var company = $("#company").val()
 		var price = $("#price").val()
+		var quantity = $("#price").val()
 		var client = $("#clientName").val()
 		var date = $("#date").val()
 		var broker = $("#broker").val()
