48ce1f438cf899dbdde2dc8a6b1e99258c48125b
Elizabeth Rosso <lizprogrammer@intracitygeeks.org>
2018-06-19 19:56:18 -0400

    Validate Stocks

--- scripts/validatestocks.js
+++ scripts/validatestocks.js
@@ -1,14 +1,41 @@
 //var myUrl = "https://api.iextrading.com/1.0/ref-data/symbols" ;
 
-var stockTicker = "FAILURE";
+//$(document).ready(function(){
 
-var myUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockTicker + "&interval=1min&apikey=HUWV206VIKKM6UB0"
+//	document.getElementById("submit").addEventListener("click", function(e){
+//        e.preventDefault()//prevent page refresh
+//
+//	    checkTicker();
+//
+//	});
 
-var results = $.getJSON(myUrl, callbackFuncWithData);
+    function checkTicker(){
+        var stockTicker = $("#symbol").val();
+
+        var myUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stockTicker + "&interval=1min&apikey=HUWV206VIKKM6UB0"
+
+        $.getJSON(myUrl, callbackFuncWithData);
+
+        console.log(myUrl);
+
+        //console.log('checkTicker() linked');
+    }
 
 
-function callbackFuncWithData(data)
-{
-    console.log(data);
- // do some thing with data
-}
+
+    function callbackFuncWithData(data)
+    {
+
+        console.log("hi");
+        console.log(data);
+
+        if(data.hasOwnProperty("Error Message"))
+        alert("Bad ticker!");
+        else
+        alert("Good ticker!");
+
+     // do some thing with data
+    }
+
+
+//})
